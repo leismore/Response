f448a12e2e9297467e6947a0742a69f1f37c9b9f
Kyle Chine <kyle@leismore.co>
2019-10-14 17:34:18 +1100

    Readme left

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,8 @@
 /**
- * Response Class
- * {
- *   res:  Express res
- * }
- *
- * Exit Code:
- *   1 - Server-side error
+ * Response class - HTTP Response class for LMOS NodeJS projects.
  */
 
-import * as express from 'express';
+import {Response} from './lib/Response';
+import {ResError} from './lib/ResError';
 
-class Response
-{
-
-  constructor(res: express.Response)
-  {
-    Object.defineProperty(this, 'res', {
-      value:        res,
-      writable:     false,
-      enumerable:   false,
-      configurable: false
-    });
-  }
-
-  errorServer(statusCode, error)
-  {
-    console.error(error);
-    this.res.sendStatus(statusCode);
-    process.exitCode = 1;
-  }
-
-  errorClient(statusCode)
-  {
-    this.res.sendStatus(statusCode);
-  }
-
-  // 403 - Forbidden
-  res403()
-  {
-    this.errorClient(403);
-  }
-
-  /**
-   * @param  {string[]} allowed - Allowed methods
-   */
-  res405(allowed)
-  {
-    this.res.set('Allow', allowed.join(', '));
-    this.errorClient(405);
-  }
-
-  // 500 - Internal Server Error
-  res500(error)
-  {
-    this.errorServer(500, error);
-  }
-
-  // 503 - Service Unavailable
-  res503(error)
-  {
-    this.errorServer(503, error);
-  }
-
-  // 204 - No Content
-  res204()
-  {
-    this.res.sendStatus(204);
-  }
-
-}
-
-export {Response};
+export {Response, ResError};
--- src/lib/Response.ts
+++ src/lib/Response.ts
@@ -5,102 +5,95 @@
  *   1 - server_error
  */
 
-import * as express    from 'express';
-import {LMError}       from '@leismore/lmerror';
-import {Res}           from './type/Res';
-import {filter_res}    from './filter_res';
-import * as HttpErrors from 'http-errors';
+import {Response as ResExpress} from 'express';
+import {LMError}                from '@leismore/lmerror';
+import {Res as ResType}         from './type/Res';
+import {filter_res}             from './filter_res';
+import {HttpError}              from 'http-errors';
 
 class Response
 {
-  protected readonly res:express.Response;
+  protected readonly res:ResExpress;
 
-  public constructor(res:express.Response)
+  public constructor(res:ResExpress)
   {
     this.res = res;
   }
 
-  public send500(error:Error)
+  /**
+   * Send HTTP response
+   * @param   response   - HTTP response data
+   * @throw   {ResError} - 1/2/3
+   */
+  public send(response:ResType):void
   {
-    console.error(String(error));
+    try
+      { response = filter_res(response); }
+    catch (e)
+      { throw e; }
+    if (response.headers !== undefined)
+      { this.res.set(response.headers); }
+    if (response.body === undefined || response.body === null)
+      { this.res.sendStatus(Number(response.statusCode)); }
+    else
+      { this.res.status(Number(response.statusCode)).send(response.body); }
+  }
+
+  public sendERROR(error:Error):void
+  {
+    if (error instanceof LMError)
+      { this.sendLMError(error); }
+    else if (error instanceof HttpError)
+      { this.sendHttpError(error); }
+    else
+      { this.sendError(error); }
+  }
+
+  private sendError(error:Error):void
+  {
+    let now = new Date();
+    console.error( now.toISOString() + ' - ' + String(error) );
     this.res.sendStatus(500);
     process.exitCode = 1;
   }
 
-  public send(response:Res)
+  private sendHttpError(error:HttpError):void
   {
-    try
-    {
-      response = filter_res(response);
-    }
-    catch (e)
-    {
-      throw e;
-    }
-    if (response.headers !== undefined)
-    {
-      this.res.set(response.headers);
-    }
-    if (response.body === undefined || response.body === null)
-    {
-      this.res.sendStatus(Number(response.statusCode));
-    }
+    let now = new Date();
+    if (error.statusCode >= 500 && error.statusCode <= 599)
+      { console.error( now.toISOString() + ' - ' + String(error) ); }
+    if (error.headers !== undefined)
+      { this.res.set(error.headers); }
+    if (error.expose === true)
+      { this.res.status(error.statusCode).send(error.message); }
     else
-    {
-      this.res.status(Number(response.statusCode)).send(response.body);
-    }
+      { this.res.sendStatus(error.statusCode); }
+    if (error.statusCode >= 500 && error.statusCode <= 599)
+      { process.exitCode = 1; }
   }
 
-  private sendError(error:HttpErrors.HttpError)
-  {
-    if (error.statusCode === undefined)
-    {
-      
-    }
-  }
-
-  public sendErr(error:Error)
-  {
-    if (error instanceof LMError)
-    {
-      this.sendLMError(error);
-    }
-    else
-    {
-      this.sendError(error);
-    }
-  }
-
-
-
-  private sendLMError(error:LMError)
+  private sendLMError(error:LMError):void
   {
     if (error.response === undefined)
     {
-      this.send500(error);
-      return;
-    }
-    if (Number(error.response.statusCode) <= 500 && Number(error.response.statusCode) <= 599)
-    {
-      console.error(String(error));
-    }
-    if (error.response.headers !== undefined)
-    {
-      this.res.set(error.response.headers);
-    }
-    if (error.response.body === undefined || error.response.body === null)
-    {
-      this.res.sendStatus(Number(error.response.statusCode));
+      console.error( String(error) );
+      this.res.sendStatus(500);
+      process.exitCode = 1;
     }
     else
     {
-      this.res.status(Number(error.response.statusCode)).send(error.response.body);
+      if ( Number(error.response.statusCode) >= 500 && Number(error.response.statusCode) <= 599 )
+        { console.error(String(error)); }
+      if (error.response.headers !== undefined)
+        { this.res.set(error.response.headers); }
+      if (error.response.body === undefined || error.response.body === null)
+        { this.res.sendStatus(Number(error.response.statusCode)); }
+      else
+        { this.res.status(Number(error.response.statusCode)).send(error.response.body); }
+      if ( Number(error.response.statusCode) >= 500 && Number(error.response.statusCode) <= 599 )
+        { process.exitCode = 1; }
     }
   }
-
-
-
-
 
 }
 
